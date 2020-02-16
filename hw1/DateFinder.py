d1aea3266645086626928ad207eb2c9e7da56cbe
Nikki Kyllonen <kyllo089@umn.edu>
2020-02-15 18:31:54 -0600

    hw1: 1.0 precision, 0.87 recall, 0.93 F1 -- submitting

--- hw1/DateFinder.py
+++ hw1/DateFinder.py
@@ -48,9 +48,21 @@ doc = codecs.open("dev.txt", encoding='cp1251').read()
 
 # 2. Define your regular expression in the string pattern.  The VERBOSE flag allows you to 
 # leverage multi-line strings, whitespace, and comments within a regex definition.
-pattern = r"""
-    \b\d+. \d+. \d+\b     # A very dumb first pass
-    """
+s1 = r"(?:\b[0-9]{1,2}[.-]{1}\w+[.-]{1}[0-9]+\b)"       # #-month.# amd #.month.#
+s2 = r"(?:\b[0-9]+[/]+[0-9]+[/]+[0-9]{2,}\b)"           # #/#/#
+s3 = r"(?:\b[JFMASOND][^rf][a-z]{3,9}\s[0-9]+\b)"       # month # -- false positives matching incompleted dates such as # month #
+#s3 = r"(?:\b[JFMASOND][aepuc][a-z]{3,9}\s[0-9]+\b)"
+#s3 = r"(?:\b[JFMASOND][aepuc][nbrylgptvc][a-z]*\s[0-9]+\b)"
+s4 = r"(?:[A-Z][a-z]{2,8}\s[0-9]{1,2}[tsrn]?[htd]?.\s[0-9]{4})" # month day(th, etc.) year
+
+s5 = r"(?:\b[0-9]{1,2}\s[A-Z][a-z]+\s[0-9]{4}\b)"       # day month year -- no false positives
+s6 = r"(?:\b[A-z][a-z]+.[-][0-9]{1,2}[-][0-9]{4}\b)"    # month -day-year -- account for extra space
+#s6 = r"(?:\b[A-z][a-z]+.*[-].*[0-9]{1,2}.*[-].*[0-9]{4}\b)"  # month-day-year -- account for extra spaces between dashes
+
+#pattern = r"{}|{}|{}".format(s1,s2,s4)
+#pattern = r"{}|{}|{}|{}".format(s1,s2,s3,s4)
+pattern = r"{}|{}|{}|{}|{}".format(s1,s2,s3,s4,s5)
+#pattern = r"{}|{}|{}|{}|{}|{}".format(s1,s2,s3,s4,s5,s6)
 
 regex = re.compile(pattern, re.VERBOSE)
 
