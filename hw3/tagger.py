7c44453d8fb77b460de10d2e56f789b9a6737f69
Nikki Kyllonen <kyllo089@umn.edu>
2020-04-05 13:30:05 -0500

    hw3: added 10+ regular expressions to get dev_set over 50% accuracy

--- hw3/tagger.py
+++ hw3/tagger.py
@@ -2,6 +2,9 @@ import nltk, re
 from nltk.tag.brill import Template, Pos, Word
 from nltk.corpus import brown
 
+# My computer requires that I download items from nltk
+# I think it might have to do with my version of Python?
+nltk.download('brown')
 
 ## Training and testing data:
 data = brown.tagged_sents(categories=['news', 'editorial'])
@@ -34,14 +37,28 @@ def get_words(tagged_sentence):
 # assigns the singular noun tag NN to every token.
 
 patterns = [
-    (r'.*ing$', 'VBG'),				  # gerunds
-	(r'.*ed$', 'VBD'),				  # simple past
-	(r'.*es$', 'VBZ'),				  # 3rd singular present
-	(r'.*ould$', 'MD'),				  # modals
-	(r'.*\'s$', 'NN$'),				  # possessive nouns
-	(r'.*s$', 'NNS'),				  # plural nouns
-	(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),                  # cadinal numbers
-	(r'.*', 'NN'), 				          # nouns (default)
+        (r'(?:\.|\?|\!)', '.'),                         # sentence terminator
+        (r'(?:[Bb][Ee]\b)', 'BE'),                      # verb 'to be'
+        (r'\b(([Aa]+[Nn]*)|([Nn][Oo])|([Tt][Hh][Ee])|([Yy][Ee])|([Ee]very))\b', 'AT'), # articles
+        (r'(?:[Ww]ere[^n])','BED'),                     # verb 'to be' past tense 2nd sing
+        (r'\b(?:[Ww][Aa][Ss])\b', 'BEDZ'),              # verb 'to be' past tenst 1st/3rd sing
+        (r'\b(?:[Aa][Mm])\b', 'BEM'),                   # verb 'to be' present tense 1st sing
+        (r'\b(?:[Ii][Ss])\b', 'BEZ'),                   # verb 'to be' present tense 3rd sing
+        (r'\b(?:([Aa][Nn][Dd])|([Oo][Rr])|([Bb]ut)|([Pp]lus)|([Nn]*[Ee]ither)|([Nn]or)|([Yy]et)|([Mm]inus))\b','CC'),                                        # conjuctions, coordinating
+        (r'\b(?:[Bb][Ee]+[Nn])\b','BEN'),               # verb 'to be', past part
+        (r'\b(?:([Ff][Oo][Rr])|([Ii][Nn]([Tt][Oo])*)|([Oo]([Ff]|[Nn]|(ut)|(ver)))|([Bb]([Yy]|efore|etween))|((([Cc]onsider)|([Rr]egard)|([Aa]ccord)|([Ii]nclud))ing)|([Aa]((mong)|(gainst)|([Ss])|(fter)|([Tt])))|([Tt](([Oo])|(hrough)|(han)|(oward)))|([Ww]ith(out)*)|([Uu]((nder)|(pon)))|([Dd](uring|espite))|([Ss]ince)|([Pp]er)|([Ee]xcept))\b','IN'),          # prepositions 
+        (r'\b(?:([Nn](one|othing|obody|o-one|othin)|([Ss](omething|omeone))|([Aa](nyone|nybody|nything)))|([Ee]very(thing|body|one))|[Oo]ne)\b','PN'),      # pronoun, nominal
+        (r'\b(?:([Ii][Tt])|([Tt]*[Ss]*[Hh][Ee]+))\b','PPS'),    # pronoun, personal, nominative
+
+        # original patterns
+        (r'.*ing$', 'VBG'),				# gerunds
+	(r'.*ed$', 'VBD'),				# simple past
+	(r'.*es$', 'VBZ'),				# 3rd singular present
+	(r'.*ould$', 'MD'),				# modals
+	(r'.*\'s$', 'NN$'),				# possessive nouns
+	(r'.*s$', 'NNS'),				# plural nouns
+	(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),                # cadinal numbers
+	(r'.*', 'NN'), 				        # nouns (default)
 	]
 
 regexp_tagger = nltk.RegexpTagger(patterns)
@@ -51,19 +68,19 @@ regexp_tagger = nltk.RegexpTagger(patterns)
 ## Part 2: Transformation-based learning and tagging
 
 # Define rule templates
-templates = [
-            Template(Pos([-1])),              # previous  POS tag
-            Template(Pos([-1]), Word([0]))    # previous POS tag + current word
-             ]
+#templates = [
+#            Template(Pos([-1])),              # previous  POS tag
+#            Template(Pos([-1]), Word([0]))    # previous POS tag + current word
+#             ]
+#
+#
+## Train a error-driven, transformation-based tagger
+#tt = nltk.BrillTaggerTrainer(regexp_tagger, templates, trace=3)
+#brill_tagger = tt.train(train_data, max_rules=25)
 
 
-# Train a error-driven, transformation-based tagger
-tt = nltk.BrillTaggerTrainer(regexp_tagger, templates, trace=3)
-brill_tagger = tt.train(train_data, max_rules=25)
-
-
-## Part 3: Evalutation
-
+## Part 3: Evaluation
+print("Regexp_tagger accuracy with dev_data: {}".format(regexp_tagger.evaluate(dev_data)))
 
 
 ## A unigram baseline tagger:
